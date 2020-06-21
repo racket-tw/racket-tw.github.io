2bd2d25110ae46a31b95e311eb09e1d9f22f288c
dannypsnl <dannypsnl@gmail.com>
2020-06-21 20:55:54 +0800

    home page

--- home.html.pm
+++ home.html.pm
@@ -1,3 +1,12 @@
 #lang pollen
 
-嗨，這裡是 racket.tw，這個網站的目標就是在台灣推廣 racket 這個
+◊h1{Racket Taiwan}
+
+嗨，這裡是 racket.tw，我的目標就是在台灣推廣 racket 這個大家應該不怎麼熟悉的程式語言。
+目前教學暫時會著重在寫給已經有程式經驗的讀者，對初學者就比較抱歉了QQ，畢竟目前只有我一個人弄。
+但初學者可以讀 ◊link["https://mitpress.mit.edu/sites/default/files/sicp/index.html"]{Structure and Interpretation of Computer Programs(SICP)} 這本書
+，或是 ◊link["https://mitpress.mit.edu/books/little-schemer-fourth-edition"]{The Little Schemer}，兩本都非常適合學習 scheme/racket 家族。
+
+◊h3{源頭}
+
+racket 源於 PLT Scheme 這個專案，至今也仍然遵循 R7RS 這個 scheme 標準並擴展，某種程度上說他們是同一個語言也還行XD。
---
+++ pollen.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(provide (all-defined-out))
+
+(define (link href . elements)
+  `(a ((href ,href)) ,@elements))
+
+(define author "Lîm Tsú-thuàn")
