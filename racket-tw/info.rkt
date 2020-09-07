0cac5aa6206d3114a7ed10e4ad0802c5a590392b
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-09-07 17:58:55 +0800

    another appoarch

--- racket-tw/info.rkt
+++
@@ -1,7 +0,0 @@
-#lang info
-(define collection "racket-tw")
-(define deps '("base"))
-(define build-deps '("scribble-lib"
-                     "racket-doc"))
-(define scribblings '(("scribblings/racket-tw.scrbl" ())))
-(define pkg-authors '(dannypsnl))
